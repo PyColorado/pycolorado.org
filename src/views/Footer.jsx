ce3faae4618129d63f159b9546ea08c251c192a2
Frank Valcarcel <frank@cuttlesoft.com>
2019-03-04 07:57:57 -0700

    typo in twitter link

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => (
     <Contact>
       <Inner>
         <ContactText>Say <a href="mailto:hello@pycolorado.org">Hello</a> or find us on{' '}
-          <a href="https://twiiter.com/pycolorado">Twitter</a> & <a href="https://slack.pycolorado.com">Slack</a>
+          <a href="https://twitter.com/pycolorado">Twitter</a> & <a href="https://slack.pycolorado.com">Slack</a>
         </ContactText>
 
         <FooterBottom>
